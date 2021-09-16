73a6d46ea4278f8a84cc86192a6c05d2b9ec162c
sofronov-kod <konstantinsofronov02@gmail.com>
2021-09-17 01:07:10 +0300

    config

--- config/config.py
+++ config/config.py
@@ -1,6 +1,11 @@
-BOT_TOKEN: str = ""
-SPOTIFY_ID: str = ""
-SPOTIFY_SECRET: str = ""
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN: str = os.getenv('BOT_TOKEN')
+# SPOTIFY_ID: str = ""
+# SPOTIFY_SECRET: str = ""
 
 BOT_PREFIX = "$"
 
